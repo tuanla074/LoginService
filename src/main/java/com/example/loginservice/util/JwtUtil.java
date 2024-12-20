1b4969ebfe8263168cf371a1a3b9193d490fbf06
tuanla074 <tuan.la074@gmail.com>
2024-12-19 23:44:30 -0500

    Login logout and register service

--- src/main/java/com/example/loginservice/util/JwtUtil.java
+++ src/main/java/com/example/loginservice/util/JwtUtil.java
@@ -3,13 +3,15 @@ package com.example.loginservice.util;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
 @Component
 public class JwtUtil {
-    private final String SECRET_KEY = "TzUwsEvGnlBVqDaXUqJQWLtKDyZUcoccWYLL3pKbUn4=y"; // Use a strong key
+    @Value("${jwt.secret}")
+    private String secret;// Use a strong key
     private final long EXPIRATION_TIME = 1000 * 60 * 60 * 10; // 10 hours
 
     public String generateToken(Long userId, String username) {
@@ -18,12 +20,12 @@ public class JwtUtil {
                 .claim("userId", userId) // Add userId as a claim
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .signWith(SignatureAlgorithm.HS256, secret)
                 .compact();
     }
 
     public Claims extractAllClaims(String token) {
-        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
     }
 
     public String extractUsername(String token) {
